bf6b04ed92f0cfad36295324acfba904a255ce34
Ivo Correia de Melo <ivocorreiademelo@macbookair-2.home>
2026-04-09 22:05:53 +0200

    modif

--- public/index.php
+++ public/index.php
@@ -1,14 +1,4 @@
 <?php
-echo "<pre>";
-var_dump([
-  'REQUEST_URI' => $_SERVER['REQUEST_URI'] ?? null,
-  'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'] ?? null,
-  'PHP_SELF' => $_SERVER['PHP_SELF'] ?? null,
-  'base' => $base ?? null,
-  'uri_initial' => $uri ?? null,
-]);
-echo "</pre>";
-exit;
 require_once dirname(__DIR__) . '/core/bootstrap.php';
 /**
  * BASE
@@ -24,19 +14,16 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if ($base !== '' && strpos($uri, $base) === 0) {
     $uri = substr($uri, strlen($base));
 }
-
 /**
  * normalisation
  */
 $uri = trim($uri, '/');
-
 /**
  * enlève index.php proprement
  */
 if (str_starts_with($uri, 'index.php')) {
     $uri = substr($uri, strlen('index.php'));
 }
-
 $uri = trim($uri, '/');
 
 /**
